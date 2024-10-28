b9b2432e5afbb53fb350e30601577fe584612d5f
zhizhi985 <98744474+zhizhi985@users.noreply.github.com>
2024-10-28 17:38:33 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,6 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git prplmesh https://github.com/prplfoundation/prplMesh-openwrt.git'
+echo 'src-git prplmesh https://github.com/prplfoundation/prplMesh-openwrt.git' >>feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
