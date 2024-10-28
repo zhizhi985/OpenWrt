877c8fb923f40bb094121ba381e235cca04c1103
zhizhi985 <98744474+zhizhi985@users.noreply.github.com>
2024-10-28 19:13:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/vernesong/OpenClash' >>feeds.conf.default
